70f2fff2d2ddc4fff0618e942dcb29713310bcd3
TowerPeng <281283646@qq.com>
2026-01-25 22:53:20 +0800

    二叉树

---
+++ com.tower.leetcode/src/main/java/com/towerpeng/practice/LevelOrder102.java
@@ -0,0 +1,72 @@
+package com.towerpeng.practice;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：[[3],[9,20],[15,7]]
+ * 示例 2：
+ *
+ * 输入：root = [1]
+ * 输出：[[1]]
+ * 示例 3：
+ *
+ * 输入：root = []
+ * 输出：[]
+ */
+public class LevelOrder102 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        LinkedList<List<Integer>> result = new LinkedList<>();
+        if(root==null){
+            return result;
+        }
+        Queue<TreeNode> que = new LinkedList<>();
+        //头结点入列
+        que.offer(root);
+        //队列不为空
+        while(!que.isEmpty()){
+            List<Integer> itemList = new ArrayList<Integer>();
+            //本层的元素数量
+            int len = que.size();
+            while(len>0){
+                TreeNode tempNode = que.poll();
+                itemList.add(tempNode.val);
+                //下一层元素入列
+                if(tempNode.left!=null){
+                    que.offer(tempNode.left);
+                }
+                if(tempNode.right!=null){
+                    que.offer(tempNode.right);
+                }
+                len--;
+            }
+            result.add(itemList);
+        }
+        return result;
+    }
+
+}
---
+++ com.tower.leetcode/src/main/java/com/towerpeng/practice/RightSideView199.java
@@ -0,0 +1,82 @@
+package com.towerpeng.practice;
+
+
+import java.util.*;
+
+/**
+ *
+ * 给定一个二叉树的 根节点 root，想象自己站在它的右侧，按照从顶部到底部的顺序，返回从右侧所能看到的节点值。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：root = [1,2,3,null,5,null,4]
+ *
+ * 输出：[1,3,4]
+ *
+ * 解释：
+ *
+ *
+ *
+ * 示例 2：
+ *
+ * 输入：root = [1,2,3,4,null,null,null,5]
+ *
+ * 输出：[1,3,4,5]
+ *
+ * 解释：
+ *
+ *
+ *
+ * 示例 3：
+ *
+ * 输入：root = [1,null,3]
+ *
+ * 输出：[1,3]
+ *
+ * 示例 4：
+ *
+ * 输入：root = []
+ *
+ * 输出：[]
+ */
+public class RightSideView199 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if(root==null){
+            return result;
+        }
+        //头结点入列
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int sz=queue.size();
+            for(int i=0;i<sz;i++){
+                TreeNode node=queue.poll();
+                //最后一个元素添加到结果
+                if(i==sz-1) result.add(node.val);
+                if(node.left!=null) queue.offer(node.left);
+                if(node.right!=null) queue.offer(node.right);
+            }
+        }
+        return result;
+    }
+
+
+
+
+}
