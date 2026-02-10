66dc6e9eef0c2fb845b4741f16899d42991af5f6
彭涛 <https//116005@gitlab.wywk.cn>
2026-02-10 14:12:26 +0800

    动态规划，背包问题，难！

---
+++ com.tower.leetcode/src/main/java/com/towerpeng/practice/dynamic/bag/FindTargetSumWays494.java
@@ -0,0 +1,111 @@
+package com.towerpeng.practice.dynamic.bag;
+
+/**
+ * 给你一个非负整数数组 nums 和一个整数 target 。
+ * 向数组中的每个整数前添加 '+' 或 '-' ，然后串联起所有整数，可以构造一个 表达式 ：
+ * 例如，nums = [2, 1] ，可以在 2 之前添加 '+' ，在 1 之前添加 '-' ，然后串联起来得到表达式 "+2-1" 。
+ * 返回可以通过上述方法构造的、运算结果等于 target 的不同 表达式 的数目。
+ * 示例 1：
+ * 输入：nums = [1,1,1,1,1], target = 3
+ * 输出：5
+ * 解释：一共有 5 种方法让最终目标和为 3 。
+ * -1 + 1 + 1 + 1 + 1 = 3
+ * +1 - 1 + 1 + 1 + 1 = 3
+ * +1 + 1 - 1 + 1 + 1 = 3
+ * +1 + 1 + 1 - 1 + 1 = 3
+ * +1 + 1 + 1 + 1 - 1 = 3
+ * 示例 2：
+ * 输入：nums = [1], target = 1
+ * 输出：1
+ *
+ * @Author: 彭涛
+ * @Date: 2026/2/10 11:49
+ */
+public class FindTargetSumWays494 {
+
+    public int findTargetSumWays(int[] nums, int target) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) sum += nums[i];
+
+        //如果target的绝对值大于sum，那么是没有方案的
+        if (Math.abs(target) > sum) return 0;
+        //如果(target+sum)除以2的余数不为0，也是没有方案的
+        if ((target + sum) % 2 == 1) return 0;
+
+        int bagSize = (target + sum) / 2;
+        int[] dp = new int[bagSize + 1];
+        dp[0] = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = bagSize; j >= nums[i]; j--) {
+                dp[j] += dp[j - nums[i]];
+            }
+        }
+        return dp[bagSize];
+    }
+
+    public int findTargetSumWays1(int[] nums, int target) {
+
+        // 01背包应用之“有多少种不同的填满背包最大容量的方法“
+        // 易于理解的二维数组解法及详细注释
+
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+
+        // 注意nums[i] >= 0的题目条件，意味着sum也是所有nums[i]的绝对值之和
+        // 这里保证了sum + target一定是大于等于零的，也就是left大于等于零（毕竟我们定义left大于right）
+        if(sum < Math.abs(target)){
+            return 0;
+        }
+
+        // 利用二元一次方程组将left用target和sum表示出来（替换掉right组合），详见代码随想录对此题的分析
+        // 如果所求的left数组和为小数，则作为整数数组的nums里的任何元素自然是没有办法凑出这个小数的
+        if((sum + target) % 2 != 0) {
+            return 0;
+        }
+
+        int left = (sum + target) / 2;
+
+        // dp[i][j]：遍历到数组第i个数时， left为j时的能装满背包的方法总数
+        int[][] dp = new int[nums.length][left + 1];
+
+        // 初始化最上行（dp[0][j])，当nums[0] == j时（注意nums[0]和j都一定是大于等于零的，因此不需要判断等于-j时的情况），有唯一一种取法可取到j，dp[0][j]此时等于1
+        // 其他情况dp[0][j] = 0
+        // java整数数组默认初始值为0
+        if (nums[0] <= left) {
+            dp[0][nums[0]] = 1;
+        }
+
+        // 初始化最左列（dp[i][0])
+        // 当从nums数组的索引0到i的部分有n个0时（n > 0)，每个0可以取+/-，因此有2的n次方中可以取到j = 0的方案
+        // n = 0说明当前遍历到的数组部分没有0全为正数，因此只有一种方案可以取到j = 0（就是所有数都不取）
+        int numZeros = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 0) {
+                numZeros++;
+            }
+            dp[i][0] = (int) Math.pow(2, numZeros);
+
+        }
+
+        // 递推公式分析：
+        // 当nums[i] > j时，这时候nums[i]一定不能取，所以是dp[i - 1][j]种方案数
+        // nums[i] <= j时，num[i]可取可不取，因此方案数是dp[i - 1][j] + dp[i - 1][j - nums[i]]
+        // 由递推公式可知，先遍历i或j都可
+        for(int i = 1; i < nums.length; i++) {
+            for(int j = 1; j <= left; j++) {
+                if(nums[i] > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i - 1][j - nums[i]];
+                }
+            }
+        }
+
+
+        return dp[nums.length - 1][left];
+
+    }
+}
