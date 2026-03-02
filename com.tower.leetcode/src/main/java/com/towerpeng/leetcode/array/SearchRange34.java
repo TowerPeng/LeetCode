4beb2bea814691ae3f802307be87db8480b77609
彭涛 <https//116005@gitlab.wywk.cn>
2026-03-02 16:56:48 +0800

    力扣

---
+++ com.tower.leetcode/src/main/java/com/towerpeng/leetcode/array/SearchRange34.java
@@ -0,0 +1,72 @@
+package com.towerpeng.leetcode.array;
+
+/**
+ * 给你一个按照非递减顺序排列的整数数组 nums，和一个目标值 target。请你找出给定目标值在数组中的开始位置和结束位置。
+ * 如果数组中不存在目标值 target，返回 [-1, -1]。
+ * 你必须设计并实现时间复杂度为 O(log n) 的算法解决此问题。
+ * 示例 1：
+ * 输入：nums = [5,7,7,8,8,10], target = 8
+ * 输出：[3,4]
+ * 示例 2：
+ * 输入：nums = [5,7,7,8,8,10], target = 6
+ * 输出：[-1,-1]
+ * 示例 3：
+ * 输入：nums = [], target = 0
+ * 输出：[-1,-1]
+ *
+ * @Author: 彭涛
+ * @Date: 2026/3/2 10:02
+ */
+public class SearchRange34 {
+
+
+    public int[] searchRange(int[] nums, int target) {
+        int[] result = new int [2];
+        result[0] = -1;
+        result[1] = -1;
+        if(nums.length==0){
+            return result;
+        }
+        int rightBorder = getRightBorder(nums,target);
+        int leftBorder = getLeftBorder(nums,target);
+        if(leftBorder==-100 || rightBorder==-100){
+            return result;
+        }
+        if (rightBorder - leftBorder > 1) return new int[]{leftBorder + 1, rightBorder - 1};
+        return result;
+    }
+
+    private int getLeftBorder(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int leftBorder = -100;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            //寻找左边界
+            if (nums[mid] >= target) {
+                right = mid - 1;
+                leftBorder = right;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return leftBorder;
+    }
+
+    private int getRightBorder(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int rightBorder = -100;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            //寻找左边界
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+                rightBorder = left;
+            }
+        }
+        return rightBorder;
+    }
+}
