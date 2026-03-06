d34d22535c96906afe1f35ca6f61f9e949999b02
TowerPeng <281283646@qq.com>
2026-03-06 09:13:52 +0800

    字节题目

---
+++ com.tower.leetcode/src/main/java/com/towerpeng/bytedance/OneNumber.java
@@ -0,0 +1,28 @@
+package com.towerpeng.bytedance;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OneNumber {
+
+    public static int singleNumber(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey();
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int [] nums = {2,2,1};
+        System.out.println(singleNumber(nums));
+    }
+}
